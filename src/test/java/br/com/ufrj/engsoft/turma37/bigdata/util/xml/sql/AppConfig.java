c2abfec18d16b7549d536b4c4c3dd2791f5fdfab
ramiralves <ramiralves@gmail.com>
2021-12-02 19:56:31 -0300

    Delete src/test/java/br/com/ufrj/engsoft/turma37/bigdata directory

--- src/test/java/br/com/ufrj/engsoft/turma37/bigdata/util/xml/sql/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.ufrj.engsoft.turma37.bigdata.util.xml.sql;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"br.com.ufrj.engsoft.turma37.bigdata.util.xml.sql"})
-public class AppConfig {	
-}
